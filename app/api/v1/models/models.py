5940c0dcd98d211bb2f637b370c6187f830c7526
kburudi <bkevery@gmail.com>
2018-12-27 01:07:57 +0300

    [starts #162851353]Added admin user to models

---
+++ app/api/v1/models/models.py
@@ -0,0 +1,9 @@
+Users = [
+    {
+        "username":"admin",
+        "password": "admin-001",
+        "email": "admin@admin.com",
+        "id" : "user-000",
+        "public_id": "tt55yfgg366sjjks"
+    }
+]
