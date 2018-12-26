11b0aa4bd36f57062729c7dc8d668b90c4d019ec
kburudi <bkevery@gmail.com>
2018-12-27 02:27:51 +0300

    [feature #162851353] Updated validations

--- app/api/v1/utils/validations.py
+++ app/api/v1/utils/validations.py
@@ -3,7 +3,7 @@ from flask import jsonify
 import re
 
 EMAIL_REGEX = re.compile(r'(\w+[.|\w])*@(\w+[.])*\w+')
-PASSWORD_REGEX = re.compile(r'[A-Za-z0-9@#$%^&+=]{6,}')
+PASSWORD_REGEX = re.compile(r'[A-Za-z0-9@#$]{6,}')
 
 class ValidateUser():
     """ This class validates data input by a user for creating an account"""
@@ -21,6 +21,7 @@ class ValidateUser():
         for user in Users:
             available_users.append(user['username'])
         
+        print(available_users)
         if self.user_data['username'] in available_users:
             return False
         
