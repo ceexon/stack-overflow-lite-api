c296915ad62af14ed8fcb68f5bb3b7edcf562bed
kburudi <bkevery@gmail.com>
2018-12-27 14:55:46 +0300

    [feature #162853158] Registered question blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,7 @@ create_app = Flask(__name__)
 create_app.config['SECRET_KEY'] = "uudye78tgde6refs55w7iwtsj"
 
 from app.api.v1.views.user_views import user_mod
+from app.api.v1.views.question_views import v1_mod
 
 create_app.register_blueprint(user_mod, url_prefix="/api/v1/auth")
+create_app.register_blueprint(v1_mod, url_prefix="/api/v1/auth")
