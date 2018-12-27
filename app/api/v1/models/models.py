0eb3fada58ff94987d4b26217b594069874deb45
kburudi <bkevery@gmail.com>
2018-12-27 14:53:03 +0300

    [starts #162853158] Added Questions to models

--- app/api/v1/models/models.py
+++ app/api/v1/models/models.py
@@ -8,3 +8,13 @@ Users = [
         "admin" : True
     }
 ]
+
+Questions = [
+    {
+        "q_id": "quiz-000",
+        "user-id" : "user-000",
+        "title" : "What is stack overflow lite",
+        "text" : "What is the main importance of stack overflow lite? How does it help the users?",
+        "time" : "27th Dec 2018, 1450hrs"
+    }
+]
