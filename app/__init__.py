6b31e24c042009bb0898f3b20455d540a4b3eb91
kburudi <bkevery@gmail.com>
2018-12-27 15:01:11 +0300

    [feature #162853158]Updated question blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,4 @@ from app.api.v1.views.user_views import user_mod
 from app.api.v1.views.question_views import v1_mod
 
 create_app.register_blueprint(user_mod, url_prefix="/api/v1/auth")
-create_app.register_blueprint(v1_mod, url_prefix="/api/v1/auth")
+create_app.register_blueprint(v1_mod, url_prefix="/api/v1")
