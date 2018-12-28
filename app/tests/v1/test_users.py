7e344e26d805956d4f9daf00d1aa3b2ee8ca567d
kburudi <bkevery@gmail.com>
2018-12-28 16:06:07 +0300

    [finishes #162868390]Added user login tests All passing

--- app/tests/v1/test_users.py
+++ app/tests/v1/test_users.py
@@ -41,10 +41,22 @@ class BaseTest(unittest.TestCase):
             "email":"trevburudi.com"
         }
 
+        self.data7 = {
+            "username":"admin",
+            "password" :"admin-003",
+            "email":"trevburudi.com"
+        }
+        
+        self.data8 = {
+            "username":"adminnn",
+            "password" :"admin-003",
+            "email":"trevburudi.com"
+        }
+
     def tearDown(self):
         pass
 
-class TestUser(BaseTest):
+class TestUserSignUp(BaseTest):
     def test_no_signup_info(self):
         response = self.client.post('api/v1/auth/signup', data=json.dumps(self.data1),content_type="application/json")
         result = json.loads(response.data.decode('utf-8'))
@@ -60,7 +72,7 @@ class TestUser(BaseTest):
     def test_empty_signup_fieldvalues(self):
         response = self.client.post('api/v1/auth/signup', data=json.dumps(self.data4),content_type="application/json")
         result = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(result["message"],"All fields must be filled")
+        self.assertIn(result["message"],"All fields must be filled")
         self.assertEqual(response.status_code, 400)
 
     def test_signup_invalid_password(self):
@@ -74,12 +86,51 @@ class TestUser(BaseTest):
         result = json.loads(response.data.decode('utf-8'))
         self.assertEqual(result["message"],"Invalid email format")
         self.assertEqual(response.status_code, 400)
+    
+    def test_signup_username_taken(self):
+        response = self.client.post('api/v1/auth/signup', data=json.dumps(self.data7),content_type="application/json")
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(result["message"],"Username is already taken")
+        self.assertEqual(response.status_code, 403)
 
     def test_signup_successful(self):
         response = self.client.post('api/v1/auth/signup', data=json.dumps(self.data2),content_type="application/json")
         self.assertEqual(response.status_code, 201)
 
+class TestUserLogin(BaseTest):
+    def test_no_login_info(self):
+        response = self.client.post('api/v1/auth/login', data=json.dumps(self.data1),content_type="application/json")
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(result["message"],"No data found")
+        self.assertEqual(response.status_code, 400)
 
+    def test_missing_login_fields(self):
+        response = self.client.post('api/v1/auth/login', data=json.dumps(self.data3),content_type="application/json")
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(result["message"],"Missing either the username or password fields")
+        self.assertEqual(response.status_code, 400)
+
+    def test_empty_login_fieldvalues(self):
+        response = self.client.post('api/v1/auth/login', data=json.dumps(self.data4),content_type="application/json")
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(result["message"],"All fields must be filled")
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_login_pass(self):
+        response = self.client.post('api/v1/auth/login', data=json.dumps(self.data7), content_type="application/json")
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(result["message"], "Invalid password")
+        self.assertEqual(response.status_code, 401)
+    
+    def test_invalid_login_username(self):
+        response = self.client.post('api/v1/auth/login', data=json.dumps(self.data8), content_type="application/json")
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(result["message"], "username not found")
+        self.assertEqual(response.status_code, 401)
+
+    def test_login_successful(self):
+        response = self.client.post('api/v1/auth/login', data=json.dumps(self.data2),content_type="application/json")
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
