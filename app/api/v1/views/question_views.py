b4f01b690de56d86678670be03a48917198c064e
kburudi <bkevery@gmail.com>
2018-12-28 21:14:16 +0300

    [finishes #162858160]Answering user upadtes answer

--- app/api/v1/views/question_views.py
+++ app/api/v1/views/question_views.py
@@ -11,7 +11,7 @@ def post_question(current_user):
     data = request.get_json()
     try:
         if not data:
-            return jsonify({"message" : "Cannot be empt"}), 400
+            return jsonify({"message" : "Cannot be empty"}), 400
 
         if data["title"] == "" or data["text"] == "":
             return jsonify({"message" : "Both title and text description are required"}), 400
@@ -59,7 +59,7 @@ def answer_question(current_user, question_id):
     data = request.get_json()
     try:
         if not data:
-            return jsonify({"message" : "Cannot be empt"}), 400
+            return jsonify({"message" : "Cannot be empty"}), 400
 
         if data["text"] == "":
             return jsonify({"message" : "Answer text description is required"}), 400
@@ -103,9 +103,107 @@ def answer_question(current_user, question_id):
     data["ans_id"] = latest_ans_id
     data["q_id"] = the_question["q_id"]
     data["time"] = time.strftime("%d/%m/%Y, %H:%M")
-    data["accepted"] = False
+    data["accepted"] = ""
 
     Answers.append(data)
 
     return jsonify({the_question["text"]: data})
+
+@v1_mod.route('/question/<question_id>/answer/<answer_id>', methods=['PUT'])
+@token_required
+def update_and_accept_answer(current_user, question_id, answer_id):
+    """ Endpoint to enable a question asker to accept an answer and a user answering the question to update their answer"""
+    data = request.get_json()
+    if not data:
+        return jsonify({"message" : "user only viewed the answer"}), 200
+
+    """ Checked who is the logged in user """
+    logged_user = {}
+    for user in Users:
+        if user["public_id"] == current_user:
+            logged_user = user
+    
+    if not logged_user:
+        return jsonify({"message" : "user not found"}), 401
+
+    """ Fetch the question """
+    the_question = {}
+    q_ids = []
+    for question in Questions:
+        q_ids.append(question["q_id"])
+        if question["q_id"][len("quiz-00"):] == question_id:
+            the_question = question
+
+    """ validate question_id """
+    q_id = "quiz-00" + question_id
+    if q_id in q_ids:
+        pass
+    else:
+        return jsonify({"Invalid Question Id" : "Question not found"}), 404
+    
+    """ Fetch the answer """
+    the_answer = {}
+    ans_ids = []
+    for answer in Answers:
+        ans_ids.append(answer["ans_id"])
+        if answer["ans_id"][len("ans-00"):] == answer_id:
+            the_answer = answer
+
+    """ validate answer_id """
+    ans_id = "ans-00" + answer_id
+    if ans_id in ans_ids:
+        pass
+    else:
+        return jsonify({"Invalid Answer Id" : "Answer not found"}), 404
+
+    """ validate question/answer pair """
+    if the_question["q_id"] != the_answer['q_id']:
+        return jsonify({"message" : "Inavalid question answer pair"}), 400
+
+    """ determine whether the logged in user is asked or answered the question """
+    ask_ans = ''
+    if the_question["username"] == logged_user["username"]:
+        ask_ans = "ask"
+    elif the_answer["username"] == logged_user["username"]:
+        ask_ans = 'ans'
+    else:
+        return jsonify({"message" : "Can't modify an answer you did not write nor can you accept an answer to a question you did not ask."}), 401
+    if ask_ans == 'ask':
+        """ Ensure asker only accepts or rejects answer """
+        try:
+            if data["text"] != the_answer["text"]:
+                return jsonify({"Permission denied" : "You can only accept or reject the answer!! nothing else!!"}), 403
+        except:
+            data["text"] = the_answer["text"]
+        try:
+            if data["accepted"] == the_answer["accepted"]:
+                return jsonify({"message" : "user only viewed the answer"}), 200
+        except:
+            data["accepted"] = the_answer["accepted"]
+
+        
+        """ User accepts or rejects answer """
+        if data["accepted"] == "false":
+            return jsonify({"rejection" : "Answer has been rejected"}), 201
+        elif data["accepted"] == "true":
+            return jsonify({"acceptance" : "Answer has been Accepted"}), 201
+        else:
+            return jsonify({"message" : "user only viewed the answer"}), 200
+
+    elif ask_ans == 'ans':
+        """ Ensure user only updates answer """
+        try:
+            if data["accepted"] != the_answer["accepted"]:
+                return jsonify({"Permission denied" : "You can only update the answer!! nothing else!!"}), 403
+        except:
+            data["accepted"] = the_answer["accepted"]
+        try:
+            if data["text"] == the_answer["text"]:
+                return jsonify({"message" : "user only reviewed the answer"}), 200
+        except:
+            data["text"] = the_answer["text"]
+
+        """ Answer has been updated"""
+        return jsonify({"message" : "Answer has been successfully updated"}), 201
+        
 
