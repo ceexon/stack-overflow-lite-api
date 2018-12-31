a10a9f75e0b022663fd10d6814b2b070019239ca
kburudi <bkevery@gmail.com>
2018-12-31 12:15:50 +0300

    [feature #162892218] Added status code to question views successful question

--- app/api/v1/views/question_views.py
+++ app/api/v1/views/question_views.py
@@ -5,16 +5,17 @@ import time
 
 v1_mod = Blueprint('QA', __name__)
 
+
 @v1_mod.route('/question', methods=['POST'])
 @token_required
 def post_question(current_user):
     data = request.get_json()
     try:
         if not data:
-            return jsonify({"message" : "Cannot be empty"}), 400
+            return jsonify({"message": "Cannot be empty"}), 400
 
         if data["title"] == "" or data["text"] == "":
-            return jsonify({"message" : "Both title and text description are required"}), 400
+            return jsonify({"message": "Both title and text description are required"}), 400
     except:
         return jsonify({"message": "Either title and text data fields are missing"}), 400
 
@@ -22,9 +23,9 @@ def post_question(current_user):
     for user in Users:
         if user["public_id"] == current_user:
             logged_user = user
-            
+
     if not logged_user:
-        return jsonify({"message" : "user not found"}), 401
+        return jsonify({"message": "user not found"}), 401
 
     data["username"] = logged_user["username"]
     latest_quiz = Questions[-1]
@@ -34,14 +35,16 @@ def post_question(current_user):
     latest_quiz_id = 'quiz-00' + str(latest_quiz_id)
     data["q_id"] = latest_quiz_id
     data["time"] = time.strftime("%d/%m/%Y, %H:%M")
-    
+
     Questions.append(data)
 
-    return jsonify({"New Question" : data})
+    return jsonify({"New Question": data}), 201
+
 
 @v1_mod.route('/question', methods=['GET'])
 def get_all_questions():
     return jsonify({"Questions": Questions})
+
 
 @v1_mod.route('/question/<question_id>', methods=['GET'])
 def get_specific_question(question_id):
@@ -49,9 +52,10 @@ def get_specific_question(question_id):
         q_id = question['q_id']
         q_id = q_id[len('quiz-00'):]
         if q_id == question_id:
-            return jsonify({"Question " + question_id : question}), 200
+            return jsonify({"Question " + question_id: question}), 200
 
-    return jsonify({"Invalid Question Id" : "Question not found"}), 404
+    return jsonify({"Invalid Question Id": "Question not found"}), 404
+
 
 @v1_mod.route('/question/<question_id>/answer', methods=['POST'])
 @token_required
@@ -59,10 +63,10 @@ def answer_question(current_user, question_id):
     data = request.get_json()
     try:
         if not data:
-            return jsonify({"message" : "Cannot be empty"}), 400
+            return jsonify({"message": "Cannot be empty"}), 400
 
         if data["text"] == "":
-            return jsonify({"message" : "Answer text description is required"}), 400
+            return jsonify({"message": "Answer text description is required"}), 400
     except:
         return jsonify({"message": "Answer text data field is missing"}), 400
 
@@ -70,9 +74,9 @@ def answer_question(current_user, question_id):
     for user in Users:
         if user["public_id"] == current_user:
             logged_user = user
-    
+
     if not logged_user:
-        return jsonify({"message" : "user not found"}), 401
+        return jsonify({"message": "user not found"}), 401
 
     data["username"] = logged_user["username"]
 
@@ -88,7 +92,7 @@ def answer_question(current_user, question_id):
     if q_id in q_ids:
         pass
     else:
-        return jsonify({"Invalid Question Id" : "Question not found"}), 404
+        return jsonify({"Invalid Question Id": "Question not found"}), 404
 
     for user in Users:
         if user['public_id'] == current_user:
@@ -109,17 +113,18 @@ def answer_question(current_user, question_id):
 
     return jsonify({the_question["text"]: data})
 
+
 @v1_mod.route('/question/<question_id>', methods=['DELETE'])
 @token_required
-def delete_question(current_user,question_id):
+def delete_question(current_user, question_id):
     """ Checked who is the logged in user """
     logged_user = {}
     for user in Users:
         if user["public_id"] == current_user:
             logged_user = user
-    
+
     if not logged_user:
-        return jsonify({"message" : "user not found"}), 401
+        return jsonify({"message": "user not found"}), 401
 
     """Get the question"""
     the_question = {}
@@ -134,13 +139,14 @@ def delete_question(current_user,question_id):
     if q_id in q_ids:
         pass
     else:
-        return jsonify({"Invalid Question Id" : "Question not found"}), 404
+        return jsonify({"Invalid Question Id": "Question not found"}), 404
 
     if logged_user["username"] != the_question["username"]:
-        return jsonify({"message" : "You cannot delete a question you did not ask"}), 401
+        return jsonify({"message": "You cannot delete a question you did not ask"}), 401
 
     Questions.remove(the_question)
-    return jsonify({"Question has been deleted" : str(len(Questions)) + " questions remaining"}), 200
+    return jsonify({"Question has been deleted": str(len(Questions)) + " questions remaining"}), 200
+
 
 @v1_mod.route('/question/<question_id>/answer/<answer_id>', methods=['PUT'])
 @token_required
@@ -148,16 +154,16 @@ def update_and_accept_answer(current_user, question_id, answer_id):
     """ Endpoint to enable a question asker to accept an answer and a user answering the question to update their answer"""
     data = request.get_json()
     if not data:
-        return jsonify({"message" : "user only viewed the answer"}), 200
+        return jsonify({"message": "user only viewed the answer"}), 200
 
     """ Checked who is the logged in user """
     logged_user = {}
     for user in Users:
         if user["public_id"] == current_user:
             logged_user = user
-    
+
     if not logged_user:
-        return jsonify({"message" : "user not found"}), 401
+        return jsonify({"message": "user not found"}), 401
 
     """ Fetch the question """
     the_question = {}
@@ -172,8 +178,8 @@ def update_and_accept_answer(current_user, question_id, answer_id):
     if q_id in q_ids:
         pass
     else:
-        return jsonify({"Invalid Question Id" : "Question not found"}), 404
-    
+        return jsonify({"Invalid Question Id": "Question not found"}), 404
+
     """ Fetch the answer """
     the_answer = {}
     ans_ids = []
@@ -187,11 +193,11 @@ def update_and_accept_answer(current_user, question_id, answer_id):
     if ans_id in ans_ids:
         pass
     else:
-        return jsonify({"Invalid Answer Id" : "Answer not found"}), 404
+        return jsonify({"Invalid Answer Id": "Answer not found"}), 404
 
     """ validate question/answer pair """
     if the_question["q_id"] != the_answer['q_id']:
-        return jsonify({"message" : "Inavalid question answer pair"}), 400
+        return jsonify({"message": "Inavalid question answer pair"}), 400
 
     """ determine whether the logged in user is asked or answered the question """
     ask_ans = ''
@@ -200,43 +206,40 @@ def update_and_accept_answer(current_user, question_id, answer_id):
     elif the_answer["username"] == logged_user["username"]:
         ask_ans = 'ans'
     else:
-        return jsonify({"message" : "Can't modify an answer you did not write nor can you accept an answer to a question you did not ask."}), 401
+        return jsonify({"message": "Can't modify an answer you did not write nor can you accept an answer to a question you did not ask."}), 401
     if ask_ans == 'ask':
         """ Ensure asker only accepts or rejects answer """
         try:
             if data["text"] != the_answer["text"]:
-                return jsonify({"Permission denied" : "You can only accept or reject the answer!! nothing else!!"}), 403
+                return jsonify({"Permission denied": "You can only accept or reject the answer!! nothing else!!"}), 403
         except:
             data["text"] = the_answer["text"]
         try:
             if data["accepted"] == the_answer["accepted"]:
-                return jsonify({"message" : "user only viewed the answer"}), 200
+                return jsonify({"message": "user only viewed the answer"}), 200
         except:
             data["accepted"] = the_answer["accepted"]
 
-        
         """ User accepts or rejects answer """
         if data["accepted"] == "false":
-            return jsonify({"rejection" : "Answer has been rejected"}), 201
+            return jsonify({"rejection": "Answer has been rejected"}), 201
         elif data["accepted"] == "true":
-            return jsonify({"acceptance" : "Answer has been Accepted"}), 201
+            return jsonify({"acceptance": "Answer has been Accepted"}), 201
         else:
-            return jsonify({"message" : "user only viewed the answer"}), 200
+            return jsonify({"message": "user only viewed the answer"}), 200
 
     elif ask_ans == 'ans':
         """ Ensure user only updates answer """
         try:
             if data["accepted"] != the_answer["accepted"]:
-                return jsonify({"Permission denied" : "You can only update the answer!! nothing else!!"}), 403
+                return jsonify({"Permission denied": "You can only update the answer!! nothing else!!"}), 403
         except:
             data["accepted"] = the_answer["accepted"]
         try:
             if data["text"] == the_answer["text"]:
-                return jsonify({"message" : "user only reviewed the answer"}), 200
+                return jsonify({"message": "user only reviewed the answer"}), 200
         except:
             data["text"] = the_answer["text"]
 
         """ Answer has been updated"""
-        return jsonify({"message" : "Answer has been successfully updated"}), 201
-        
-
+        return jsonify({"message": "Answer has been successfully updated"}), 201
